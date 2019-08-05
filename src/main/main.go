7b7501a6f7fae002d5ffc240083477c7b2dda7fb
hihey <lumia418@live.com>
2019-08-05 14:29:35 +0800

    初始化提交

--- src/main/main.go
+++ src/main/main.go
@@ -1,17 +1,16 @@
-package main 
+package main
 
 import (
-    "net/http"
-    "fmt"
+	"fmt"
+	"net/http"
 )
 
 // Default Request Handler
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>Hello %s!</h1>", r.URL.Path[1:])
+	fmt.Fprintf(w, "<h1>Hello %s!</h1>", r.URL.Path[1:])
 }
 
 func main() {
-    http.HandleFunc("/", defaultHandler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", defaultHandler)
+	http.ListenAndServe(":8080", nil)
 }
-
